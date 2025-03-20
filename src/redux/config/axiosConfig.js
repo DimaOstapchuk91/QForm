f2817233527c814bcadcaa16c482da3855964619
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-03-20 15:07:10 +0200

    change axios base url, contect render server

--- src/redux/config/axiosConfig.js
+++ src/redux/config/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const questionnairesAPI = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://qform-back.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
